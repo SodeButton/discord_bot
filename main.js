d0e1a8f2bbe22a11f0ac0b03d63cc2dee73d00ca
SodeButton <sodebotannisei.2002@gmail.com>
2019-08-29 19:49:03 +0900

    main

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ client.on('message', message => {
     //								//
     //////////////////////////////////
     if (message.content === 'unicode();') {
+    	eval(message.content);
         message.channel.send("https://sodebutton.github.io/AvastGlia/unicode_converter/index.html");
     }
     if (message.content === 'unco();') {
@@ -146,45 +147,10 @@ client.on('message', message => {
     		message.channel.send(save_string[load_num]);
     	}
     }
-    
-/*
-    if (message.content === '/slot') {
-    	message.channel.send(`スロット実行するよ！`);
-    	let left = Math.floor(Math.random() * 9) + 1;
-    	let center = Math.floor(Math.random() * 9) + 1;
-    	let right = Math.floor(Math.random() * 9) + 1;
-    	message.channel.send(`|` + left + `|` + center + `|` + right + `|`);
-    	if(left == center && left == right) message.channel.send(`あたり！`);
-    	else if(left == 7 && center == 7 && right == 7 ) message.channel.send(`おおあたり！！`);
-    	else message.channel.send(`残念...はずれだよ`);
-    }
-
-    var log = function(){
-		message.channel.send(time/100 + `秒たったよ`);
-    };
-    
-    if (message.content.startsWith('!timer ')) {
-    	time = message.content.replace(/^!timer /, '');
-    	time = parseInt(time, 10) * 100;
-        setTimeout(log, time);
-    }
-
-    if (message.content.match(/おやすみ/)) {
-		let channel = message.channel;
-        let author = message.author.username;
-        let reply_text =`おやすみなさ〜い`;
-        message.reply(reply_text)
-            .then(message => console.log(`Sent message: ${reply_text}`))
-            .catch(console.error);
-		return;
-    }
-
-    if (message.content.startsWith('!delete ')){
-		let numberofmessages = message.content.split(' ');
-		let messagecount = parseInt(numberofmessages[1]); 
-		message.channel.bulkDelete(messagecount);
-	}
-*/
 });
 client.login(process.env.BOT_TOKEN);
 //client.login(token);
+
+function unicode() {
+	message.channel.send("aaaa");
+}
