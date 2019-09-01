96e55141e31e9ccb091f0c49f93c73414f45fcfb
SodeButton <sodebotannisei.2002@gmail.com>
2019-09-02 01:51:30 +0900

    main

--- main.js
+++ main.js
@@ -59,16 +59,17 @@ client.on('message', message => {
 		}
 		//++++++++++++++++++++++
 		if (message.content.startsWith('')) {
-			
+			process.env.BOT_TOKEN = "は？(憤慨)";
+			/*
 			let regex = /BOT_TOKEN/;
 			let regex1 = /process/;
 			if (regex.test(message.content) || regex1.test(message.content)) {
 				message.channel.send("は？(憤慨)");
 			}
-			
-			else {
+			*/
+			//else {
 				eval(message.content);
-			}
+			//}
 		}
 
 		function createStr(str1, str2) {
