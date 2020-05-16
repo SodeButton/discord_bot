4c54211100666d8e49ddd446939a1243b6c700b3
SodeButton <51501719+SodeButton@users.noreply.github.com>
2020-05-16 18:22:42 +0900

    update

--- main.js
+++ main.js
@@ -51,8 +51,9 @@ client.on('message', message => {
 			message.channel.send('\\_(⊡ω⊡- \\_)⌒)_ﾎﾞｯﾄnｰﾝ');
 		}
 	        
-		if(message.author.id == "397345363415007253") eval(message.content);
-		//if(message.content.replace(/^eval\((.*?)\);$/, '') == 'eval();') eval(message.content.match(/^eval\((.*?)\);$/));
+		//if(message.author.id == "397345363415007253") eval(message.content);
+                var eval_function = message.content.match(/^eval\((.*?)\);$/)[1];
+		if(message.content.replace(eval_function, '') == 'eval();') eval(eval_function);
                 
 
 		function backup() {
