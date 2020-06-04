f273774983754bd4f6628faa47e78639327a5bdb
SodeButton <51501719+SodeButton@users.noreply.github.com>
2020-06-04 11:54:32 +0900

    upfate

--- main.js
+++ main.js
@@ -68,13 +68,21 @@ client.on('message', message => {
 		}
 	        
 		if(message.content.startsWith('eval();')) {
-			
+			var token_error = `
+				var process = {
+					env : {
+						BOT_TOKEN : "環境変数も表示できないよーっだ！"
+					}
+				};
+				var client = {
+					token: "表示できないよーっだ！"
+				};
+			`;
 			say_messages = "";
 			log_messages = "";
-                    	eval(message.content.replace(/^eval\(\);/, ''));
-                        if(!say_messages.match(process.env.BOT_TOKEN))
-			        message.channel.send(say_messages);
-			if(log_messages != "" && !log_messages.match(process.env.BOT_TOKEN)) {
+                    	eval(token_error + message.content.replace(/^eval\(\);/, ''));
+			message.channel.send(say_messages);
+			if(log_messages != "") {
 				message.channel.send({
 					embed: {
 						title: 'Log Messages',
